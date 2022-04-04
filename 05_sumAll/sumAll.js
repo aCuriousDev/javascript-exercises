800e98f7c02ff822d4b951d3da281620920ef506
aCuriousDev <quentinmarius@gmail.com>
2022-04-04 04:44:25 +0200

    Done sumAll with arithmetic

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let sumResult;
+    let max;
+    let min;
 
+    if ((int1 < 0 || int2 < 0) || ((typeof int1 !== 'number' )|| (typeof int2 !== 'number')) ) {
+        return 'ERROR'
+    }
+
+    if (int1 >= int2) {
+        max = int1;
+        min = int2;
+        sumResult = min;
+    } else {
+        max = int2;
+        min = int1;
+        sumResult = min;
+    }
+
+    sumResult = (0.5 * max) * (max + 1);
+
+    return sumResult
 };
 
 // Do not edit below this line
