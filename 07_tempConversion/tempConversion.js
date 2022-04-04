78dc9c1945a84566e3c69812da4c834a9d18b1e7
aCuriousDev <quentinmarius@gmail.com>
2022-04-04 15:27:04 +0200

    Done tempConversion; Used Math.round with * then /

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ctof = function(temp) {
+  let convTemp = (temp * (9/5)) + 32;
+  return Math.round(convTemp * 10) / 10
 };
 
-const ctof = function() {
-
+const ftoc = function(temp) {
+  let convTemp = (temp - 32) / (9/5);
+  return Math.round(convTemp * 10) / 10
 };
 
 // Do not edit below this line
