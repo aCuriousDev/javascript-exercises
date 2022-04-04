f5e473d114b768d6b1bb421770179a49b827925b
aCuriousDev <quentinmarius@gmail.com>
2022-04-04 15:05:08 +0200

    Done leapYears with &&, || operator

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%4 === 0 && (year%100 !== 0 || year%400 ===0) ) {
+        console.log('is a leap year');
+        return true
+    } else {
+        console.log('is not a leap year');
+        return false
+    }
 };
 
 // Do not edit below this line
