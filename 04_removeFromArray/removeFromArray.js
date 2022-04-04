c7459d122598185153208c9a8b4b16260fd1dcf0
aCuriousDev <quentinmarius@gmail.com>
2022-04-04 04:05:13 +0200

    removeFromArray Done, used Spread and Filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...arg) {
+    arr = arr.filter(item => !arg.includes(item));
+    
+    return arr
 };
 
 // Do not edit below this line
